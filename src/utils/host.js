01038e3e3c4a4a6b3e0f87717f1f9dcbe2866228
shaqeebSk <shaqeeb@pentagoat.com>
2024-11-27 14:52:54 +0530

    host file changed

--- src/utils/host.js
+++ src/utils/host.js
@@ -1,3 +1,6 @@
 
 
-export const host = "http://localhost:8000";
+export const host = "https://chatapp-server-hon9.onrender.com";
+
+// https://chatapp-server-hon9.onrender.com
+// "http://localhost:8000"
