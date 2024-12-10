bcdb40e34b2c6d5cae657b15aa63e574d46b2893
shaqeebshaikh123 <90406905+Shk123s@users.noreply.github.com>
2024-12-10 17:32:36 +0530

    Update host.js

--- src/utils/host.js
+++ src/utils/host.js
@@ -1,6 +1,6 @@
 
 
-export const host = "https://chatapp-server-hon9.onrender.com";
+export const host = "https://chatapp-server-tau.vercel.app";
 
 // https://chatapp-server-hon9.onrender.com
 // "http://localhost:8000"
